317fb30afb8667d0b2ab8b242070f177595d34c7
Animesh Das <animeshd838@gmail.com>
2021-03-19 21:10:40 +0530

    first commit

---
+++ src/componenets/jumbotron/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {Inner} from './styles/jumbotron'
+
+export default function Jumbtron(){
+    return(
+        <Inner direction={direction}>
+            I am the Jumbo!
+        </Inner> 
+    )
+}
